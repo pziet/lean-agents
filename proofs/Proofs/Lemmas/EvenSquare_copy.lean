2027236110510706b92c5e3e46a69351cd6be2bb
patrickmziet <patrickzietkiewicz@gmail.com>
2025-04-02 23:48:49 +0100

    update: lots, still messy

--- proofs/Proofs/Lemmas/EvenSquare_copy.lean
+++ proofs/Proofs/Lemmas/EvenSquare_copy.lean
@@ -2,4 +2,8 @@ import Mathlib.Tactic
 import «Proofs».Definitions.isEven
 
 lemma even_square (n : ℕ) (hn : isEven n) : isEven (n * n) := by
-  sorry
+  -- sorry
+  rcases hn with ⟨k, hk⟩
+  use 2 * k * k
+  rw [hk]
+  ring
--- proofs/Proofs/Theorem.lean
+++ proofs/Proofs/Theorem.lean
@@ -4,9 +4,4 @@ import «Proofs».Lemmas.EvenSquare
 
 -- Prove that is n, m are even then n^2 + m is even
 theorem even_square_plus_even (n m : ℕ) (hn : isEven n) (hm : isEven m) : isEven (n * n + m) := by
-  -- sorry
-  rcases even_square n hn with ⟨k1, hk1⟩    -- n^2 = 2 * k1
-  rcases hm with ⟨k2, hk2⟩                 -- m = 2 * k2
-  use k1 + k2                              -- Claim: n^2 + m = 2 * (k1 + k2)
-  rw [hk1, hk2]
-  ring
+  sorry
